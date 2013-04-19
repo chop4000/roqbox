52eff551b33d9c5adcd52ad23baac495bc491759
Chris Hopkins <chrisbot4000@gmail.com>
2013-04-18 18:21:32 -0700

    First commit of all files.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Roqbox::Application.config.secret_token = '27d74ff28f107b7690547ad62dc509326114bb7832c57780b5d289178154fba8d50a0363a961807cddf8b12b5366a905f6363c105aa96db102df61f8899abbaa'
